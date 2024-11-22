ad373b1fdf1324fc522bc3ad986d11095009ff80
Zane <90309290+Zingzy@users.noreply.github.com>
2024-11-22 18:44:50 +0530

    Add pyzbar support for QR code detection and improve error handling

--- src/services/qr_processor.py
+++ src/services/qr_processor.py
@@ -1,5 +1,26 @@
-import cv2
-import numpy as np
+try:
+    from pyzbar.pyzbar import decode, ZBarSymbol
+except FileNotFoundError:
+    import sys
+    from loguru import logger
+    from tkinter import messagebox
+
+    logger.error(
+        "Failed to import pyzbar. This may be due to a missing vcredist_x64.exe file."
+    )
+    logger.error(
+        "Please download and install the Visual C++ Redistributable for Visual Studio 2013 from the following link:"
+    )
+    logger.error("https://www.microsoft.com/en-gb/download/details.aspx?id=40784")
+
+    messagebox.showerror(
+        "Import Error",
+        "Failed to import pyzbar. This may be due to a missing vcredist_x64.exe file.\n\n"
+        "Please download and install the Visual C++ Redistributable for Visual Studio 2013 from the following link:\n"
+        "https://www.microsoft.com/en-gb/download/details.aspx?id=40784",
+    )
+    sys.exit(1)
+
 from PIL import Image
 from loguru import logger
 from typing import Optional, Tuple, Union
@@ -10,13 +31,13 @@ class QRCodeProcessor:
 
     @staticmethod
     def detect_qr_code(
-        image: Optional[Union[Image.Image, np.ndarray]],
+        image: Optional[Union[Image.Image, str]],
     ) -> Tuple[Optional[str], bool]:
         """
         Detect and decode QR code from an image
 
         Args:
-            image: Image to process
+            image: Image to process or path to the image file
 
         Returns:
             Tuple of (detected data, detection success)
@@ -26,17 +47,17 @@ class QRCodeProcessor:
             return None, False
 
         try:
-            # Ensure image is in correct format
-            if isinstance(image, Image.Image):
-                image_cv: np.ndarray = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-            else:
-                image_cv: np.ndarray = image
+            if isinstance(image, str):
+                image = Image.open(image)
 
             logger.debug("Starting QR code detection")
-            detector: cv2.QRCodeDetector = cv2.QRCodeDetector()
-            data, bbox, _ = detector.detectAndDecode(image_cv)
+            decoded_objects = decode(image, symbols=[ZBarSymbol.QRCODE])
 
-            if data:
+            if decoded_objects:
+                data = decoded_objects[0].data.decode("utf-8")
+                if not data:
+                    logger.warning("Empty QR Code detected")
+                    return None, False
                 logger.success(f"QR Code detected: {data}")
                 return data, True
 
--- tests/test_qr_processor.py
+++ tests/test_qr_processor.py
@@ -40,9 +40,6 @@ def test_detect_qr_code_with_valid_gradient_qr_code_image(image_path, expected_d
 lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In facilisis laoreet turpis, sit amet elementum nulla congue eget. Duis volutpat ex at vehicula tristique. Phasellus at ligula eu nisl cursus vulputate. Donec in tincidunt urna. Mauris mollis lacus nec consequat tristique. Mauris ligula risus, dictum blandit ante vel, accumsan aliquet leo. Maecenas mauris quam, varius vitae sapien at, malesuada pharetra nulla. Cras at mauris condimentum dui congue elementum. Cras fringilla erat sit amet orci vehicula suscipit. Fusce sit amet arcu et erat venenatis molestie non id velit.  Maecenas a posuere tortor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec semper mi nibh, sagittis convallis augue posuere quis. Nunc est augue, tincidunt a vulputate at, viverra ac tortor. Integer luctus, est in tincidunt iaculis, odio mi sodales odio, sit amet pretium diam justo sit amet mi. Integer cursus, felis vel molestie commodo, arcu augue varius massa, quis finibus eros lectus id est. Donec tristique gravida hendrerit. Mauris ac quam est. Nullam in feugiat elit. Quisque egestas dolor eros, eget tincidunt turpis aliquam eu. Aliquam gravida est tempor quam elementum vehicula. Quisque non blandit arcu, eget blandit odio. Phasellus suscipit erat at massa mollis tempor."
 
 
-@pytest.mark.xfail(
-    reason="Will Fail because the current QR detection System does not support huge QR codes"
-)
 @pytest.mark.parametrize(
     "image_path, expected_data",
     [
@@ -57,15 +54,13 @@ def test_detect_qr_code_with_valid_huge_qr_code_image(image_path, expected_data)
     assert success
 
 
-@pytest.mark.xfail(
-    reason="Stylized QR detection is not supported by the current system"
-)
 @pytest.mark.parametrize(
     "image_path, expected_data",
     [
         ("tests/data/valid_stylized_qr1.png", "Valid QR Code"),
         ("tests/data/valid_stylized_qr2.png", "Valid QR Code"),
         ("tests/data/valid_stylized_qr3.png", "Valid QR Code"),
+        ("tests/data/valid_stylized_qr4.png", "Valid QR Code"),
     ],
 )
 def test_detect_qr_code_with_valid_stylized_qr_code_image(image_path, expected_data):
